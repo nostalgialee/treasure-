cb29bb14cf731bfce139ad2fc2de55f6b7539707
nostalgialee <842109551@qq.com>
2022-06-17 15:02:58 +0800

    面试题第一次提交

---
+++ basic_knowledge_of_operating_system.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2022/6/17 15:01
+# @File    : basic_knowledge_of_operating_system.py
+# Author: lee
---
+++ network_basics.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2022/6/17 15:01
+# @File    : network_basics.py
+# Author: lee
