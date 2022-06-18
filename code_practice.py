ff90bae6533d417afa6f8c450aa859f103589f80
nostalgialee <842109551@qq.com>
2022-06-18 23:05:26 +0800

    618提交

--- code_practice.py
+++ code_practice.py
@@ -276,6 +276,48 @@ alist = list(i[1] for i in ( filter(lambda x:x[0] % 2 == 1, enumerate(alist))))
 """
 23.实现一个装饰器，限制该函数被调用的频率，如10秒一次
 
+import time
+from functools import wraps
+
+def dec(func):
+
+    func_name = func.__name__
+    cache = {func_name: None}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = None
+        if cache.get(func_name) is None:
+            # 未被执行过
+            cache[func_name] = time.time()
+            result = func(*args, **kwargs)
+            print('function is running')
+
+        else:
+            start_time = cache.get(func_name)
+            now_time = time.time()
+            time_interval = now_time - start_time
+            if time_interval >= 10:
+                cache[func_name] = now_time
+                result = func(*args, **kwargs)
+
+            else:
+                print('function cant run for a while')
+        return result
+
+    return wrapper
+
+
+@dec
+def add(x, y):
+    print(x + y)
+
+
+add(1, 2)
+add(1, 3)
+time.sleep(10)
+add(3, 4)
+
 
 """
 
@@ -284,12 +326,152 @@ alist = list(i[1] for i in ( filter(lambda x:x[0] % 2 == 1, enumerate(alist))))
 
 
 
+"""
+24.实现一个装饰器，通过一次调用，使函数重复执行5次
 
+from functools import wraps
+
+def dec(func):
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = [func(*args, **kwargs) for i in range(5)]
+        return result
+    return wrapper
+
+@dec
+def add(x, y):
+    print(x + y)
+
+add(1, 2)
+
+"""
+
+
+"""
+25.请编写一个函数将ip地址转换成一个整数。
+如10.3.9.12转换成00001010 00000011 00001001 00001100，然后转换成整数
+
+"""
 
 
 
 """
-24.实现一个装饰器，通过一次调用，使函数重复执行5次 
+26.求以下代码结果
+def num():
+    return [lambda x:i*x for i in range(4)]
+print([m(2) for m in num()])
 
 """
 
+
+
+"""
+27.求以下代码的输出结果
+collapse=True
+processFunc=collapse and (lambda s:' '.join(s.split())) or (lambda s:s)
+print(processFunc('i\tam\ntest\tproject!'))
+
+collapse=False
+processFunc=collapse and (lambda s:' '.join(s.split())) or (lambda s:s)
+print(processFunc('i\tam\ntest\tproject!'))
+
+"""
+
+
+"""
+28.
+编写一个函数，找出数组中没有重复的值的和
+
+
+
+"""
+
+
+"""
+29.下面代码的执行结果是
+
+a=1
+def bar():
+    a+=3
+    
+bar()
+print(a)
+
+
+"""
+
+
+"""
+30.写一个函数，计算出以下字母所代表的数字，每个字母值不一样
+
+"""
+
+
+"""
+31.写出如下代码的输出结果
+def decorator_a(func):
+    print('Get in decorator_a')
+    def inner_a(*args, **kwargs):
+        print('Get in inner_a')
+        return func(*args, **kwargs)
+    return inner_a
+    
+def decorator_b(func):
+    print('Get in decorator_b')
+    def inner_b(*args, **kwargs):
+        print('Get in inner_b')
+        return func(*args, **kwargs)
+    return inner_b
+    
+@decorator_b #f=decorator_b(f)
+@decorator_a #f=decorator_a(f)
+def f(x):
+    print('Get in f')
+    return x * 2
+f(1)
+"""
+
+
+
+"""
+32.写出以下代码的输出结果：
+
+def test():
+    try:
+        raise ValueError('something wrong')
+    except ValueError as e:
+        print('error occured')
+        return
+    finally:
+        print('ok')
+test()
+
+"""
+
+
+"""
+33.求出以下代码的输出结果
+mydict={'a':1,'b':2}
+def func(d):
+    d['a']=0
+    return d
+    
+func(mydict)
+mydict['c']=2
+print(mydict)
+
+"""
+
+
+"""
+34.写个函数接收一个文件夹名称作为参数，
+显示文件夹中文件的路径，以及其中包含的文件夹中文件的如今
+
+
+"""
+
+
+"""
+35.输入某年某月某日，判断这是这一年的第几天
+"""
